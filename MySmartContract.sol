ca7b240accb6d985cace7a5a17d1b3f93d2da71d
Ambeshdubey <153484734+Ambeshdubey@users.noreply.github.com>
2024-07-04 07:57:19 +0530

    MySmartContract.sol

--- MySmartContract.sol
+++ MySmartContract.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MySmartContract {
+contract RequireAssertRevert {
     uint256 public value;
+    uint256 public counter;
 
     constructor(uint256 initialValue) {
         require(initialValue >= 0, "Initial value must be non-negative");
         value = initialValue;
+        counter = 0;
     }
 
     function setValue(uint256 newValue) public {
         require(newValue >= 0, "Value must be non-negative");
         value = newValue;
+    }
+
+    function incrementCounter() public {
+        counter++;
+        // Assert that the counter never overflows (for demonstration purposes)
+        assert(counter > 0);
     }
 
     function doRevert(uint256 newValue) public {
@@ -21,8 +29,8 @@ contract MySmartContract {
         value = newValue;
     }
 
-    function checkAssertion(uint256 testValue) public pure {
-        // This will always be true since uint256 cannot be negative
-        assert(testValue >= 0);
+    function triggerAssertion() public view {
+        // This assertion will always fail for demonstration purposes
+        assert(counter == 999999999);
     }
 }
